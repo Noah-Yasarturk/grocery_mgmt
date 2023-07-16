05c2e2a46ca5283e1b9f16b973517357ba3ac13b
Noah-Yasarturk <Noah.Yasarturk@gmail.com>
2023-07-16 17:56:07 -0400

    backup getRds

---
+++ lambda/getGroceryRds.py
@@ -0,0 +1,16 @@
+'''
+This code will enable us to run SQL queries to our RDS instance.
+
+Format:
+{
+    "query": "sql_query"
+}
+'''
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
