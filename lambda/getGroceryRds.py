3e18df3947f575f478461403a58867e3d210a929
Noah-Yasarturk <Noah.Yasarturk@gmail.com>
2023-07-16 18:15:51 -0400

    Validate params in getGroceryRds

--- lambda/getGroceryRds.py
+++ lambda/getGroceryRds.py
@@ -1,16 +1,24 @@
-'''
-This code will enable us to run SQL queries to our RDS instance.
-
-Format:
-{
-    "query": "sql_query"
-}
-'''
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
+   print(f"Event received: {event}")
+   resp =  validate_params(event)
+   if 'statusCode' in resp.keys():
+       return resp 
+   # Proceed with query
+   return  {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': 'Success'
     }
+
+
+def validate_params(event):
+    # Confirm that "query" is the only key
+    for k in event.keys():
+        if k != 'query':
+            print(f"Received invalid param: {k}")
+            return {
+                'statusCode': 400,
+                'body': 'Invalid params. Only query allowed'
+            }
+    return event
