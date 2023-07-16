726fd755b3261b663ca3358a3db8a899780fc25e
Noah-Yasarturk <Noah.Yasarturk@gmail.com>
2023-07-16 19:38:42 -0400

    Create db

--- lambda/getGroceryRds/getGroceryRds.py
+++ lambda/getGroceryRds/getGroceryRds.py
@@ -4,39 +4,42 @@ import pymysql
 import logging
 import sys
 
+
+logger = logging.getLogger()
+
+
 def lambda_handler(event, context):
+
     # Handle event
-    print(f"Context: {context}")
-    print(f"Event received: {event}")
+    logger.info(f"Context: {context}")
+    logger.info(f"Event received: {event}")
     resp =  validate_params(event)
     if 'statusCode' in resp.keys():
         return resp
     
-    logger = logging.getLogger()
-
-    # rds settings
-    user_name = os.environ['USER_NAME']
-    password = os.environ['PASSWORD']
-    rds_host = os.environ['RDS_HOST']
-    # db_name = os.environ['DB_NAME']
-
-    try:
-        conn = pymysql.connect(host=rds_host, user=user_name, passwd=password, connect_timeout=5)
-    except pymysql.MySQLError as e:
-        logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
-        logger.error(e)
-        sys.exit()
-
-
-    logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+    # Connect
     query = event['query']
-    first_word = query.split()[0].upper()
+    first_word = query.split()[0].upper().strip()
     results = []
-    with conn.cursor() as cursor:
+    with connect_to_rds().cursor() as cursor:
+
+        # Execute query
+        logger.info(f"Executing query: {query}")
         cursor.execute(query)
         # Handle SELECTs
         if first_word == 'SELECT':
             results = cursor.fetchall()
+        # Handle CREATEs
+        elif first_word == 'CREATE':
+            results = cursor.rowcount
+
+        # Handle unaccepted query types
+        else:
+            logger.error(f"Unacceptable query type: {first_word}")
+            return {
+                'statusCode': 400,
+                'rds_response': 'INVALID_QUERY'
+            }
 
     return  {
             'statusCode': 200,
@@ -54,3 +57,20 @@ def validate_params(event):
                 'body': 'Invalid params. Only query allowed'
             }
     return event
+
+
+
+def connect_to_rds():
+    # Connect to RDS
+    user_name = os.environ['USER_NAME']
+    password = os.environ['PASSWORD']
+    rds_host = os.environ['RDS_HOST']
+    db_name = os.environ['DB_NAME']
+    try:
+        conn = pymysql.connect(host=rds_host, user=user_name, passwd=password, database=db_name, connect_timeout=5)
+    except pymysql.MySQLError as e:
+        logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+        logger.error(e)
+        sys.exit()
+    logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+    return conn
