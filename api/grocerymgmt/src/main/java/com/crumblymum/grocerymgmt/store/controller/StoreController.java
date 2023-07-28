8d9db9211800ac90740a8389e836435bebf1e1bb
Noah-Yasarturk <Noah.Yasarturk@gmail.com>
2023-07-27 20:30:03 -0400

    Created the Create Store endpoint

--- api/grocerymgmt/src/main/java/com/crumblymum/grocerymgmt/store/controller/StoreController.java
+++ api/grocerymgmt/src/main/java/com/crumblymum/grocerymgmt/store/controller/StoreController.java
@@ -1,24 +1,39 @@
 package com.crumblymum.grocerymgmt.store.controller;
 
-import java.sql.SQLException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.crumblymum.grocerymgmt.utils.DbAccessUtil;
+import com.crumblymum.grocerymgmt.store.Store;
+import com.crumblymum.grocerymgmt.store.service.GroceryMgmtException;
+import com.crumblymum.grocerymgmt.store.service.StoreService;
 
 @RestController
 public class StoreController {
 
+	@Autowired
+	private StoreService storeService; 
+
     @GetMapping("/store/")
-	public String testStoreMapping() throws SQLException{
-		try {
-			DbAccessUtil.getConnection();
-		} catch(SQLException e) {
-			System.out.println("Couldn't connect to db. ");
-			throw new SQLException(e);
-		}
+	public String testStoreMapping() {
 		return "The store endpoint works!";
+	}
+
+	@PostMapping("/store/add")
+	public String postStore(@RequestBody Store newStore) throws GroceryMgmtException{
+		System.out.println(newStore);
+		String dbMessage = "Did not write anything to db";
+		try {
+			 dbMessage = this.storeService.writeStore(newStore);
+		} catch(GroceryMgmtException e) {
+			e.printStackTrace();
+			// TODO: how to handle?
+		}
+		return dbMessage;
+
 	}
     
 }
